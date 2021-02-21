d3f9f7187d0bdcadd9ed8355e3ca3c14e63fc9a1
Nico Proto <nicolasproto@gmail.com>
2021-02-21 16:27:18 +0100

    Rolled back changes

--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,6 @@
 Rails.application.routes.draw do
   devise_for :users
-  root to: 'pages#home'
   get '/kitchensink', to: 'pages#kitchensink' if Rails.env.development?
-  get '/paralaxslider', to: 'pages#paralaxslider'
+  root to: 'pages#home'
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 end
