0112a54363b904e2eb89b9c67c8eadb2b686f77c
Nico Proto <nicolasproto@gmail.com>
2021-02-21 15:10:55 +0100

    Added paralax slider route and view

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,7 @@
 Rails.application.routes.draw do
   devise_for :users
-  get '/kitchensink', to: 'pages#kitchensink' if Rails.env.development?
   root to: 'pages#home'
+  get '/kitchensink', to: 'pages#kitchensink' if Rails.env.development?
+  get '/paralaxslider', to: 'pages#paralaxslider'
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 end
