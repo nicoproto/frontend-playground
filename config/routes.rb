283c1d118cdbb22f03dbc671b43abeac86282f2a
Nico Proto <nicolasproto@gmail.com>
2021-02-23 11:23:09 +0100

    Added custom route prefixes

--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,7 @@
 Rails.application.routes.draw do
   devise_for :users
-  get '/kitchensink', to: 'pages#kitchensink' if Rails.env.development?
-  get '/sidebar', to: 'pages#sidebar'
+  get '/kitchensink', to: 'pages#kitchensink', as: :kitchensink
+  get '/sidebar', to: 'pages#sidebar', as: :sidebar
   root to: 'pages#home'
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 end
