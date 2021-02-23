88d069d66c4b51a6f67c825c63b6c1600d4a434b
Nico Proto <nicolasproto@gmail.com>
2021-02-23 11:23:56 +0100

    Updated devise restrictions

--- app/controllers/pages_controller.rb
+++ app/controllers/pages_controller.rb
@@ -1,7 +1,9 @@
 class PagesController < ApplicationController
-  skip_before_action :authenticate_user!, only: [ :home, :kitchensink ]
+  skip_before_action :authenticate_user!, only: [ :home, :kitchensink, :sidebar ]
 
   def home; end
 
   def kitchensink; end
+
+  def sidebar; end
 end
